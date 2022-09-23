31ba49552788a3bed10665d823a09debf115c907
yyz <yalex778@gmail.com>
2022-09-22 22:46:03 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/healthapp/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.example.healthapp.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.healthapp.data.LoginDataSource
+import com.example.healthapp.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                    loginRepository = LoginRepository(
+                            dataSource = LoginDataSource()
+                    )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
